58f327150e1879cac2c1542390f6914daa1f47bc
Your Name <your_email_here>
2026-03-17 02:11:16 +0200

    Fix output formatting

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
-print("Hello world!")
-print("Not another hello world")
-print("Well")
-print("More")
-print("Hello")
-print("World")
-print("Please, I'm begging you.")
+Hello world!
+Not another hello world
+Well
+More
+Hello
+World
+Please, I'm begging you.
